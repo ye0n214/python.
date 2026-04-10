26aebef3eb9bddd663578e706d5a9242d57b2bf0
ye0n214 <siyeon070214@gmail.com>
2026-04-10 19:58:10 +0900

    로또

--- 프로그램1.py
+++ 프로그램1.py
@@ -1,14 +1,11 @@
 import random
 
-## 함수 선언 부분 ##
 def getNumber():
     return random.randrange(1,46)
 
-## 전역 변수 선언 부분 ##
 lotto=[]
 num=0
 
-## 메인 코드 부분 ##
 print("**로또 추첨을 시작합니다.**\n");
 
 while True:
